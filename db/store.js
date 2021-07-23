2cd663466cb4264bbc69dbe44cd23657c0e48a51
twizard <trevcoons@gmail.com>
2021-07-22 20:49:00 -0700

    edit class Store and server.js

--- db/store.js
+++ db/store.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 const utils = require('utils');
-const uuid = require('uuid');
 
-const readFileAsync = util.promisify()
 
-//Make a class called Storage
+const uuidv1 = require('uuid/v1');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 class Store {
     read() {
-        return readFileAsync
+        return readFileAsync('db/db.json', 'utf8');
     }
+
+    write(note) {
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+      }
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const api = require('./routes/api');
 const html = require('./routes/html');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
