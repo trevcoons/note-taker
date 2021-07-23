3363b486e8e8e1fa07a686679dc166dc72335e8f
twizard <trevcoons@gmail.com>
2021-07-22 22:04:05 -0700

    add remove note function

--- db/store.js
+++ db/store.js
@@ -45,4 +45,12 @@ class Store {
       .then(() => newNote);
   }
   
+  removeNote(id) {
+    // Get all notes, remove the note with the given id, write the filtered notes
+    return this.getNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((filteredNotes) => this.write(filteredNotes));
+  }
 }
+
+module.exports = new Store();
