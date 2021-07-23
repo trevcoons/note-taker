c7013834c11cf6dc4949fd5c52d6980e131b1bd6
twizard <trevcoons@gmail.com>
2021-07-22 21:42:10 -0700

    add get notes function

--- db/store.js
+++ db/store.js
@@ -15,4 +15,20 @@ class Store {
     write(note) {
         return writeFileAsync('db/db.json', JSON.stringify(note));
       }
+    
+    getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+
+      // If notes isn't an array or can't be turned into one, send back a new empty array
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }
+  
 }
